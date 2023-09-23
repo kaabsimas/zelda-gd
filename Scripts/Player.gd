c7ed5b63bb9cc21fe075f0fb74ecb9ab3010fd58
Albert Melo <albert.uler@brilliantmachine.com.br>
2023-09-23 01:06:24 -0300

    Movimentos do player quase terminados

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,5 +1,11 @@
 extends CharacterBody2D
 
+@onready var animtree = $AnimationTree
+@onready var animationState = animtree.get("parameters/playback")
+
+enum player_states {MOVE, SWORD, JUMP, DEAD}
+var current_states = player_states.MOVE
+
 var input_movement = Vector2.ZERO
 var speed = 70
 
@@ -7,15 +13,34 @@ func _ready():
 	$Sword/CollisionShape2D.disabled = true
 
 func _physics_process(delta):
-	move()
+	match current_states:
+		player_states.MOVE:
+			move()
+		player_states.SWORD:
+			sword()
 
 func move():
 	input_movement = Input.get_vector('ui_left', 'ui_right', 'ui_up', 'ui_down')
 	
 	if input_movement != Vector2.ZERO:
+		animtree.set("parameters/Idle/blend_position", input_movement)
+		animtree.set("parameters/Walk/blend_position", input_movement)
+		animtree.set("parameters/Sword/blend_position", input_movement)
+		animtree.set("parameters/Jump/blend_position", input_movement)
+		animationState.travel('Walk')
 		velocity = input_movement * speed
 	
 	if input_movement == Vector2.ZERO:
+		animationState.travel('Idle')
 		velocity = Vector2.ZERO
+		
+	if Input.is_action_just_pressed("sword"):
+		current_states = player_states.SWORD
 	
 	move_and_slide()
+
+func sword():
+	animationState.travel("Sword")
+	
+func on_states_reset():
+	current_states = player_states.MOVE
